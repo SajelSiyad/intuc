b1e93d8c3269a527b416c8a5042f3d37c8f8f95d
LikhinNelliyotan <42886379+LikhinNelliyotan@users.noreply.github.com>
2024-01-05 19:12:35 +0530

    notification page added

---
+++ lib/view/notification_page/notification_page.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:intuc/utils/dynamic_sizing/dynamic_sizing.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Notification",
+          style: TextStyle(
+              fontSize: R.rw(20, context), fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        elevation: 3,
+        shadowColor: Colors.black,
+      ),
+      body: Container(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/payment_bg.png"),
+              fit: BoxFit.cover),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(R.rw(13, context)),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                R.rw(10, context),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: R.rw(10, context),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(R.rw(3, context)),
+                  decoration:
+                      const BoxDecoration(color: Color.fromARGB(20, 0, 0, 0)),
+                  child: const Text(
+                    "Today",
+                    style: TextStyle(
+                        color: Colors.black45, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(
+                    R.rw(10, context),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "You have a Event at 2:30pm today",
+                        style: TextStyle(
+                            fontSize: R.rw(15, context),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      const Text(
+                          "Event is conducted at thrissur in ABC Hall about the change in keralas Educational system"),
+                      Text(
+                        "1 hour ago",
+                        style: TextStyle(
+                            fontSize: R.rw(10, context),
+                            fontWeight: FontWeight.w300),
+                      ),
+                      SizedBox(
+                        height: R.rw(10, context),
+                      ),
+                      Text(
+                        "Today is a special day",
+                        style: TextStyle(
+                            fontSize: R.rw(15, context),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      const Text(
+                          "Event is conducted at thrissur in ABC Hall about the change in keralas Educational system"),
+                      Text(
+                        "3 hour ago",
+                        style: TextStyle(
+                            fontSize: R.rw(10, context),
+                            fontWeight: FontWeight.w300),
+                      ),
+                      SizedBox(
+                        height: R.rw(10, context),
+                      ),
+                      Text(
+                        "Its your birthday",
+                        style: TextStyle(
+                            fontSize: R.rw(15, context),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      const Text("Get your wishes from your community"),
+                      Text(
+                        "4 hour ago",
+                        style: TextStyle(
+                            fontSize: R.rw(10, context),
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(R.rw(3, context)),
+                  decoration:
+                      const BoxDecoration(color: Color.fromARGB(20, 0, 0, 0)),
+                  child: const Text(
+                    "Yesterday",
+                    style: TextStyle(
+                        color: Colors.black45, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                SizedBox(
+                  height: R.rw(10, context),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(R.rw(10, context)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Today is Rahul Gandhi’s Birthday",
+                        style: TextStyle(
+                            fontSize: R.rw(15, context),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      const Text("Get your wishes from your community"),
+                      Text(
+                        "4 hour ago",
+                        style: TextStyle(
+                            fontSize: R.rw(10, context),
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/trade_union_page/trade_uion_page.dart
+++ lib/view/trade_union_page/trade_uion_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intuc/utils/dynamic_sizing/dynamic_sizing.dart';
+import 'package:intuc/view/notification_page/notification_page.dart';
 import 'package:intuc/view/widgets/widgets.dart';
 
 class TradeUnionPage extends StatelessWidget {
@@ -18,50 +19,69 @@ class TradeUnionPage extends StatelessWidget {
         elevation: 2,
         shadowColor: Colors.amberAccent,
       ),
-      body: Container(
-        height: MediaQuery.sizeOf(context).height,
-        width: MediaQuery.sizeOf(context).width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/bg 1.png"), fit: BoxFit.cover),
-        ),
-        child: ListView.separated(
-            padding: EdgeInsets.all(R.rw(5, context)),
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: EdgeInsets.all(R.rw(5, context)),
-                child: Container(
-                  padding: EdgeInsets.all(R.rw(10, context)),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(
+      body: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const NotificationPage(),
+            ),
+          );
+        },
+        child: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg 1.png"), fit: BoxFit.cover),
+          ),
+          child: ListView.separated(
+              padding: EdgeInsets.all(R.rw(5, context)),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.all(
+                    R.rw(3, context),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(
                       R.rw(10, context),
                     ),
-                  ),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        "assets/images/Logo.png",
-                        scale: 9,
-                      ),
-                      SizedBox(
-                        width: R.rw(15, context),
-                      ),
-                      Flexible(
-                        child: Text(
-                          unions[index],
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          R.rw(10, context),
                         ),
-                      ),
-                      const Icon(Icons.arrow_forward_ios_outlined)
-                    ],
+                        boxShadow: [
+                          BoxShadow(
+                            offset: const Offset(0, 1),
+                            blurRadius: R.rw(3, context),
+                          )
+                        ]),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/Logo.png",
+                          scale: 9,
+                        ),
+                        SizedBox(
+                          width: R.rw(15, context),
+                        ),
+                        Flexible(
+                          child: Text(
+                            unions[index],
+                          ),
+                        ),
+                        const Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox();
-            },
-            itemCount: unions.length),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox();
+              },
+              itemCount: unions.length),
+        ),
       ),
     );
   }
